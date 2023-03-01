9326bb86bc1d5c8e3d41b92c604a354a440bfe76
Thiago Panini <panini.development@gmail.com>
2023-03-01 18:32:01 -0300

    chore: setup inicial do repositório

---
+++ README.md
@@ -0,0 +1,20 @@
+<div align="center">
+    <br><img src="https://github.com/ThiagoPanini/cloudgeass/blob/feature/repo-first-organization/docs/assets/imgs/header-readme.png?raw=true" alt="gluesnake-logo">
+</div>
+
+<div align="center">  
+  <br>
+  
+  [![PyPI](https://img.shields.io/pypi/v/gluesnake?color=purple)](https://pypi.org/project/gluesnake/)
+  ![PyPI - Downloads](https://img.shields.io/pypi/dm/gluesnake?color=purple)
+  ![PyPI - Status](https://img.shields.io/pypi/status/gluesnake?color=purple)
+  ![GitHub commit activity](https://img.shields.io/github/commit-activity/m/ThiagoPanini/gluesnake?color=purple)
+  ![GitHub Last Commit](https://img.shields.io/github/last-commit/ThiagoPanini/gluesnake?color=purple)
+  <br>
+
+  ![CI workflow](https://img.shields.io/github/actions/workflow/status/ThiagoPanini/gluesnake/ci-main.yml?label=ci)
+
+</div>
+
+> **Note**
+> Trabalho em construção
---
+++ setup.py
@@ -0,0 +1,41 @@
+"""
+Arquivo de configuração da biblioteca.
+"""
+# Importando bibliotecas
+from setuptools import setup, find_packages
+
+# Lendo README.md
+with open("README.md", "r", encoding='utf-8') as f:
+    __long_description__ = f.read()
+
+# Criando setup
+setup(
+    name='gluesnake',
+    version='0.0.1',
+    author='Thiago Panini',
+    author_email='panini.development@gmail.com',
+    packages=find_packages(),
+    install_requires=[],
+    license='MIT',
+    description='Funcionalidades Spark para facilitar a criação de jobs Glue na AWS',
+    long_description=__long_description__,
+    long_description_content_type="text/markdown",
+    url='https://github.com/ThiagoPanini/gluesnake',
+    keywords='Cloud, AWS, Python, Spark, pyspark',
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Natural Language :: Portuguese (Brazilian)",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ],
+    python_requires=">=3.0.0"
+)
