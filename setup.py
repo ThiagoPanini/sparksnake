96e20836fcafcf945fccf1f660935141be97811a
Thiago Panini <panini.development@gmail.com>
2023-03-08 19:14:36 -0300

    feat: release v0.1.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open("README.md", "r", encoding='utf-8') as f:
 # Criando setup
 setup(
     name='gluesnake',
-    version='0.0.4',
+    version='0.1.0',
     author='Thiago Panini',
     author_email='panini.development@gmail.com',
     packages=find_packages(),
